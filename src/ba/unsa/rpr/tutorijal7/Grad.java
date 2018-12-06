ed0cf513fca884c5e5ca2ec889055520e9aa7ca3
vseremet1 <vseremet1@etf.unsa.ba>
2018-12-06 11:48:43 +0100

    zadatak1

--- src/ba/unsa/rpr/tutorijal7/Grad.java
+++ src/ba/unsa/rpr/tutorijal7/Grad.java
@@ -1,27 +1,39 @@
 package ba.unsa.rpr.tutorijal7;
 
+import java.util.Arrays;
+
 public class Grad {
 
 
     String naziv;
     int brojStanovnika;
-    double [] temperature ;
+    double[] temperature;
+    int brojMjerenja;
 
-    public Grad(String naziv, double[] temperature) {
-        this.naziv=naziv;
-        this.temperature=temperature;
-        this.brojStanovnika=0;
+    public Grad(String naziv, double[] temperature, int brojMjerenja) {
+        this.naziv = naziv;
+        this.temperature = temperature;
+        this.brojStanovnika = 0;
+        this.brojMjerenja = brojMjerenja;
     }
 
     // mora imati konstruktor bez parametara i da svi atributi moraju posjedovati settere i gettere po specifikaciji
 
 
-    public void Grad ()
-    {
+    public void Grad() {
         naziv = "";
-        brojStanovnika=0;
-        temperature=null;
+        brojStanovnika = 0;
+        brojMjerenja = 0;
+        temperature = null;
 
+    }
+
+    public int getBrojMjerenja() {
+        return brojMjerenja;
+    }
+
+    public void setBrojMjerenja(int brojMjerenja) {
+        this.brojMjerenja = brojMjerenja;
     }
 
     public String getNaziv() {
@@ -46,5 +58,15 @@ public class Grad {
 
     public void setTemperature(double[] temperature) {
         this.temperature = temperature;
+    }
+
+    @Override
+    public String toString() {
+        return "Grad{" +
+                "naziv='" + naziv + '\'' +
+                ", brojStanovnika=" + brojStanovnika +
+                ", temperature=" + Arrays.toString(Arrays.copyOf(temperature, brojMjerenja)) +
+                ", brojMjerenja=" + brojMjerenja +
+                '}';
     }
 }
--- src/ba/unsa/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/rpr/tutorijal7/Tutorijal.java
@@ -1,7 +1,52 @@
-package  ba.unsa.rpr.tutorijal7;
+package ba.unsa.rpr.tutorijal7;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Tutorijal {
 
+
+    public static ArrayList<Grad> ucitajGradove() {
+        String s;
+        ArrayList<Grad> gradovi = new ArrayList<>();
+        double[] temp = new double[1000];
+        int i, broj;
+        broj = 0;
+
+        Scanner ulaz;
+
+        try {
+            ulaz = new Scanner(new FileReader("mjerenja.txt"));
+            while (ulaz.hasNext()) {
+                broj=0;
+                s = ulaz.nextLine();
+                String[] niz = s.split(",");
+                i = 1;
+                while (i < niz.length) {
+                    temp[i - 1] = Double.parseDouble(niz[i]);
+                    i++;
+                    broj++;
+                }
+
+                Grad g = new Grad(niz[0], temp, broj);
+                gradovi.add(g);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return gradovi;
+
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+        ArrayList<Grad> gradovi;
+        gradovi = ucitajGradove();
+        for (Grad g : gradovi) {
+            System.out.println(g);
+        }
     }
 }
